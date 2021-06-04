72bd494a427e8ef391943e4d45fac9445d7fc8ce
Rodrigo Machado <rodrigoborgesmachado@github.com>
2021-06-04 12:57:11 -0300

    Subindo últimas alterações

--- biomas.php
+++ biomas.php
@@ -32,16 +32,72 @@ session_start();
         					</div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-8">
-                            <h4>
-                            </h4>
-                        </div>
-                        <div class="col-sm-4">
-                        </div>
+                    <div class="row" id="biomas">
+                        
                     </div>
 			    </div>
             </div>
 		</div>
     </body>
 </html>
+
+<script>
+
+PreencheBiomas();
+
+    function PreencheBiomas(){
+        var xhr = new XMLHttpRequest();
+
+        var nomeBioma = '';
+        var quantidadeVotos = 1;
+        var dados = JSON.stringify({nomeBioma});
+        xhr.open("POST", "http://plantasnacionais.sunsalesystem.com.br/PHP/GetBiomas.php");
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.addEventListener("load", function() {
+            var erroAjax = document.querySelector("#erro-ajax");
+            if (xhr.status == 200) {
+                //TODO colocar tela de carregamento
+                PreencheBiomasTexto(JSON.parse(xhr.responseText));
+            } else {
+                alert('Não foi possível buscar os biomas.');
+            }
+        }
+        );
+
+        xhr.send(dados);
+    }
+
+    function PreencheBiomasTexto(listaBiomas){
+        document.getElementById('biomas').hidden = false;
+
+        var texto = '';
+
+        texto += '    <div class="row">';
+
+        for(i = 0; i < listaBiomas.length; i++){
+            texto += '      <div class="col-sm-8">';
+            texto += '          <input class="form-control" id="codigoBioma_' + i + '" type="hidden" value="listaBiomas[i].ID">';
+            texto += '          <br>';
+            texto += '          <h2 style="text-align: center;"> ' + listaBiomas[i].NOME + ' </h2>';
+            texto += '          <br>';
+            texto += '          <h4>';
+            texto += '          Distribuição: ' + listaBiomas[i].DISTRIBUICAO;
+            texto += '          <br>';
+            texto += '          Características: ' + listaBiomas[i].CARACTERISTICAS;
+            texto += '          <br>';
+            texto += '          Fitofisionomia: ' + listaBiomas[i].FITOFISIONOMIA;
+            texto += '          <br>';
+            texto += '          Observação: ' + listaBiomas[i].OBSERVACAO;
+            texto += '          </h4>';
+            texto += '    ' + (getCookie('LOGADO') == '1' ?
+                           `<h4 style="text-align: right;">
+                                <button class="buttonInicio" onclick="AbrirModalCadastroImagem();">Adicionar Imagens</button>
+                            </h4>` : '');
+            texto += '    </div>';
+        }
+        texto += '    </div>';
+
+        document.getElementById('biomas').innerHTML += texto;
+    }
+</script>
